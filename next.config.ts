54ffba71f4efb1164e823f2fca827549cfd2c0b1
cakesbyshiratzur <cakesbyshiratzur@gmail.com>
2025-10-05 19:51:39 -0500

    Clean up repository: remove log files and fix image locations

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/shiras-cakes' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/shiras-cakes' : '',
 };
 
 export default nextConfig;
